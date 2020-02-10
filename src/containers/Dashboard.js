b79427d1070b5bdcd28b07782febb2e05fbf482c
devarcher <archershot@hotmail.com>
2020-02-10 15:13:46 -0600

    first section complete

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,14 +1,14 @@
-import { connect } from 'react-redux'
-import Dashboard from '../components/Dashboard'
+import { connect } from "react-redux";
+import Dashboard from "../components/Dashboard";
 // import { removeCar } action here
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-        cars: state.cars
-    }
-}
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    cars: state.cars
+  };
+};
 
 // add mapDispatchToProps function here
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps)(Dashboard);
